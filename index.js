05e4a0890a9e82cc785bd68b819a9ae9ac2f41e8
Ryan Fernandes Bertaglia <ryanfernandesbertaglia@gmail.com>
2024-07-27 12:47:26 -0300

    Escolha de dia

--- index.js
+++ index.js
@@ -5,15 +5,29 @@ var columns = ["Date", "Open", "High", "Low", "Close", "Adjusted Close", "Volume
 document.getElementById("get_data").addEventListener("click", getData);
 
   async function getData() {
-    var url = `https://www.alphavantage.co/query?function=TIME_SERIES_WEEKLY_ADJUSTED&symbol=IBM&apikey=5FBPJXV8F137NKZI`; // Construct the URL
+    var url = `https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=IBM&apikey=5FBPJXV8F137NKZI`; // Construct the URL
   
       // Fetch data from the API
       let response = await fetch(url);
-      let data = await response.json();
-      console.log(data);}
-      // Display the JSON data on the page
-    /*  var json_container = document.getElementById("json_container");
-      json_container.textContent = JSON.stringify(data, null, 2);
-    i
-  }*/
-  
+      var data = await response.json();
+      console.log(data);
+      const processedData = [];
+            for (var key in data['Time Series (Daily)']) {
+                processedData.push({
+                    data: key,
+                    abertura: parseFloat(data['Time Series (Daily)'][key]['1. open']),
+                    alta: parseFloat(data['Time Series (Daily)'][key]['2. high']),
+                    baixa: parseFloat(data['Time Series (Daily)'][key]['3. low']),
+                    fechamento: parseFloat(data['Time Series (Daily)'][key]['4. close'])
+                });}
+      console.log(processedData[0]);
+      inicTabela(processedData);
+      for(let i=0; i<=14; i++){
+        getElementById
+      }
+    function inicTabela(processedData){
+      let containe = document.getElementById('table_container');
+      for(let i=0; i<=14; i++){
+        const containe = document.createElement('<tr> <td>${processed_container[i].abertura}</td> </tr>');
+      };
+    }}
